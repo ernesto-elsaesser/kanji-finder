b641cab156155adfb97ee7280c12a327d6b559e0
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2025-07-21 10:26:57 +0000

    Reduce krad to kyouiko kanji

--- build-krad.py
+++ build-krad.py
@@ -1,18 +1,40 @@
-# https://github.com/neocl/jamdict/blob/main/jamdict/data/kradfile-u.gz
-
+# %%
+import io
 import json
+import gzip
+import urllib.request
 
-with open("edict2.json") as f:
-    edict = json.load(f)
+KRAD_URL = "https://github.com/neocl/jamdict/raw/refs/heads/main/jamdict/data/kradfile-u.gz"
 
-edict_index = {k for e in edict for k in e}
+# %%
+with open("kyouiko.txt") as f:
+    kyouiko = set(f.read())
 
-with open("kradfile-u") as f:
-    lines = [l[:-1] for l in f if l[0] != "#"]
+# %%
+with urllib.request.urlopen(KRAD_URL) as response:
+    gzipped_file = io.BytesIO(response.read())
+    with gzip.open(gzipped_file, "rt") as f:
+        lines = f.readlines()
+
+# %%
+
+lines = [l[:-1] for l in lines if l[0] != "#"]
+
+# %%
+
+lines = [l for l in lines if l[0] in kyouiko]
+
+# %%
 
 lines = sorted(lines, key=len)
-krad = {l[0]: l[4:].replace(" ", "") for l in lines if l[0] in edict_index}
+
+# %%
+krad = {l[0]: l[4:].replace(" ", "") for l in lines}
 krad = {k: v for k, v in krad.items() if k != v}
+
+# %%
 
 with open("krad.json", "w") as f:
     json.dump(krad, f, indent=4, ensure_ascii=False)
+
+# %%
