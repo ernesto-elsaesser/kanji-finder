30a9e55050ba175d6599bec8c379b63808022c92
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2025-08-28 11:01:04 +0000

    Show kanji infos

--- build-krad.py
+++ build-krad.py
@@ -6,14 +6,14 @@ import gzip
 import urllib.request
 
 KRAD_URL = "https://github.com/neocl/jamdict/raw/refs/heads/main/jamdict/data/kradfile-u.gz"
-JOYO_URL = "https://raw.githubusercontent.com/NHV33/joyo-kanji-compilation/refs/heads/master/joyo.csv"
+JOUYOU_URL = "https://raw.githubusercontent.com/NHV33/joyo-kanji-compilation/refs/heads/master/joyo.csv"
 
 # %%
-with urllib.request.urlopen(JOYO_URL) as response:
+with urllib.request.urlopen(JOUYOU_URL) as response:
     csv_lines = response.read().decode("utf-8").splitlines()
     reader = csv.reader(csv_lines)
     next(reader)
-    jouyou = {r[1] for r in reader}
+    jouyou = {r[1]: r[3:] for r in reader}
 
 # %%
 with urllib.request.urlopen(KRAD_URL) as response:
@@ -23,19 +23,37 @@ with urllib.request.urlopen(KRAD_URL) as response:
 
 # %%
 
-lines = [l[:-1] for l in lines if l[0] != "#"]
+entries = []
+for line in lines:
+    kanji = line[0]
+    if kanji == "#": # comment line
+        continue
+
+    decomp = line[4:-1].replace(" ", "")
+    if decomp == kanji:
+        continue
+
+    info = jouyou.get(kanji)
+    if info is None:
+        continue
+
+    radical, strokes, grade, year, meanings, on, kun, freq, jlpt = info
+    entry = {
+        "decomp": decomp,
+        "radical": radical,
+        "meanings": meanings,
+        "on": on.split("|")[0],
+        "kun": kun.split("|")[0],
+        "strokes": int(strokes),
+        "grade": grade,
+        "jlpt": int(jlpt),
+        "freq": int(freq),
+    }
+    entries.append((int(freq), kanji, entry))
 
 # %%
 
-lines = [l for l in lines if l[0] in jouyou]
-
-# %%
-
-lines = sorted(lines, key=len)
-
-# %%
-krad = {l[0]: l[4:].replace(" ", "") for l in lines}
-krad = {k: v for k, v in krad.items() if k != v}
+krad = {k: e for _, k, e in sorted(entries)}
 
 # %%
 
