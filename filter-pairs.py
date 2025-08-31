aa0098658c03ab5a3bf808c518272e8dca84c7c7
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2025-08-31 15:31:51 +0000

    Add quiz game

--- filter-pairs.py
+++ filter-pairs.py
@@ -11,8 +11,21 @@ with open("edict.json") as f:
 
 # %%
 
-pairs = {k: v for k, v in edict.items()
-          if len(k) == 2 and k[0] in jouyou and k[1] in jouyou}
+pairs = {i: [] for i in reversed(range(1, 6))}
+
+for word in edict:
+    if len(word) != 2:
+        continue
+    info0 = jouyou.get(word[0])
+    info1 = jouyou.get(word[1])
+    if info0 is None or info1 is None:
+        continue
+    min_jlpt = min(info0["jlpt"], info1["jlpt"])
+    pairs[min_jlpt].append(word)
+
+# %%
+
+pairs = {f"N{i}": l for i, l in pairs.items()}
 
 # %%
 
