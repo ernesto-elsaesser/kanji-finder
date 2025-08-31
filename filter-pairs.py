880637016a9ad30e68a3228a1bf84b7ab20017b8
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2025-08-31 14:25:27 +0000

    Update pairs script

--- filter-pairs.py
+++ filter-pairs.py
@@ -1,20 +1,10 @@
 # %%
-import io
-import csv
 import json
-import gzip
-import urllib.request
-
-JOUYOU_URL = "https://raw.githubusercontent.com/NHV33/joyo-kanji-compilation/refs/heads/master/joyo.csv"
 
 # %%
-with urllib.request.urlopen(JOUYOU_URL) as response:
-    csv_lines = response.read().decode("utf-8").splitlines()
-    reader = csv.reader(csv_lines)
-    next(reader)
-    jouyou = {r[1]: r[3:] for r in reader}
 
-# %%
+with open("jouyou.json") as f:
+    jouyou = json.load(f)
 
 with open("edict.json") as f:
     edict = json.load(f)
