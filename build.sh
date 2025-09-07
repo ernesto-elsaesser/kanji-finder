164d8bfb8e2bcd2c5f5d707ca428d2a628855181
Ernesto Elsäßer <ernesto.elsaesser@gmail.com>
2025-09-07 10:18:38 +0000

    Update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
+echo "jouyou.json ..."
 python3 load_jouyou.py jouyou.json
+echo "edict.json ..."
 python3 load_edict.py radicals/edict.json
+echo "krad.json ..."
 python3 load_krad.py jouyou.json radicals/krad.json
+echo "pairs.json ..."
 python3 build_quiz.py jouyou.json radicals/edict.json quiz/jlpt.json quiz/pairs.json
